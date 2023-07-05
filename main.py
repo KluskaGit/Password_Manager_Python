f4c4411332f5925ef74e3fb5f8a622908c5d14d7
Kamil Kluska <kamil6kluska7@gmail.com>
2023-07-05 16:42:26 +0200

    register function

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
 import mysql.connector
+from argon2 import PasswordHasher
 
+#MENU
 def menu():
     print("""
     -------MENU-------
     1. Log in
-    2. Sign in
+    2. Register
     """)
 
+    choice=input('Option : ')
+    match choice:
+        case '1':
+            log_in()
+        case '2':
+            register()
+
+
+#Log in function
+def log_in():
+    login=input('Login: ')
+    password=input('Password: ')
+
+#Register function
+def register():
+    email=input('Your email: ')
+    password=input('Password: ')
+    r_password=input('Repeat password: ')
+
+    if password==r_password:
+        hash_pas=ph.hash(password)
+        cursor.execute(f'INSERT INTO users (email, password) values ("{email}", "{hash_pas}")')
+        conn.commit()
+        log_in()
+
+    
+
 if __name__=='__main__':
+    ph = PasswordHasher()
     
     conn = mysql.connector.connect(
         user='root', 
@@ -17,10 +47,10 @@ if __name__=='__main__':
         )
     cursor=conn.cursor()
 
-
-    while(True):
-        choice=input('Option : ')
+    register()
         
+
+
 
     
     
