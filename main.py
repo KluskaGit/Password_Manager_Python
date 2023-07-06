d919ad216c1224ce8629dd3c7a6cf79934532bf1
Kamil Kluska <kamil6kluska7@gmail.com>
2023-07-06 22:44:31 +0200

    Visual aspects

--- main.py
+++ main.py
@@ -5,29 +5,32 @@ import os
 
 #MENU
 def menu():
+    os.system('cls')
     print("""
     -------MENU-------
     1. Log in
     2. Register
     """)
 
-    choice=input('Option : ')
+    choice=input('Option: ')
     match choice:
         case '1':
             log_in()
         case '2':
             register()
         case other:
-            print('No match found')
             menu()
 
+#Second menu after log in
 def menuPass(id):
+    os.system('cls')
     print("""
     1. Display password
     2. Add new password
     3. Log out
+    4. Exit
     """)
-    choice=input('Option : ')
+    choice=input('Option: ')
     match choice:
         case '1':
             display_pas(id)
@@ -35,30 +38,58 @@ def menuPass(id):
             add_pass(id)
         case '3':
             log_out()
+        case '4':
+            quit()
         case other:
-            print('No match found')
-            menu()
+            menuPass(id)
 
+#View existing passwords
 def display_pas(id):
+    os.system('cls')
+    print('Your passwords:\n')
     cursor.execute(f'SELECT * From all_passwords where user_id={id}')
     rows=cursor.fetchall()
-    for row in rows:
-        print(f"Platform: {row['platform']}\nPassword: {row['password']}\n-------------------")
+    if rows !=[]:
+        for row in rows:
+            print(f"Platform: {row['platform']}\nPassword: {row['password']}\n-------------------")
+        print('\n1. Go back')
+        choice=input("Option: ")
+        match choice:
+            case '1':
+                menuPass(id)
+            case other:
+                display_pas(id)
+    else:
+        print('No passwords to display')
+        print('\n1. Go back')
+        choice=input("Option: ")
+        match choice:
+            case '1':
+                menuPass(id)
+            case other:
+                display_pas(id)
 
+#Adding new passwords
 def add_pass(id):
     os.system('cls')
+    print('Add new password here:\n')
     platform=input('Platform name: ')
     password=input('Password: ')
     cursor.execute(f'INSERT INTO all_passwords (user_id, platform, password) values ({id}, "{platform}", "{password}")')
     conn.commit()
     print('Password has been added')
     menuPass(id)
-    
+
+#Log out   
 def log_out():
-    pass
+    menu()
 
 #Log in function
 def log_in():
+    os.system('cls')
+    print("Log in here:\n")
+
+
     email=input('Email: ')
     password=input('Password: ')
     cursor.execute(f'SELECT * From users where email like "{email}"')
@@ -75,6 +106,9 @@ def log_in():
         
 #Register function
 def register():
+    os.system('cls')
+    print("Register here:\n")
+
     email=input('Your email: ')
     password=input('Password: ')
     r_password=input('Repeat password: ')
@@ -83,11 +117,13 @@ def register():
         hash_pas=ph.hash(password)
         cursor.execute(f'INSERT INTO users (email, password) values ("{email}", "{hash_pas}")')
         conn.commit()
-        log_in()
+        menu()
+    
 
     
 
 if __name__=='__main__':
+
     ph = PasswordHasher()
     
     conn = mysql.connector.connect(
@@ -97,7 +133,11 @@ if __name__=='__main__':
         database='m28441_passwords_manager'
         )
     cursor=conn.cursor(dictionary=True)
-    log_in()
+
+
+
+    while(True):
+        menu()
 
 
 
